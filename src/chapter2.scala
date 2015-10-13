0c4683fc9c98fbd49ccaecd4b37dd1f21da30f93
clen <clen@domeyard.com>
2015-10-13 12:41:20 -0400

    isSorted

--- src/chapter2.scala
+++ src/chapter2.scala
@@ -1,5 +1,6 @@
 import scala.annotation.tailrec
 
+// Exercise 1
 object Factorial {
     def fib(n: Int): Int = { 
         @tailrec
@@ -11,8 +12,31 @@ object Factorial {
     }
 }
 
+// Exercise 2
+object IsSorted {
+    def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = {
+        as.toList match {
+            case Nil => true
+            case _::Nil => true
+            case a::b::_ if gt(a,b) => isSorted(as.tail, gt)
+            case _ => false
+        }
+    }
+}
+
+
+
 object HelloWorld extends App {
-    1 to 10 foreach { x => println(Factorial.fib(x)) }
+    println("Fib:")
+    1 to 10 foreach { x => print(Factorial.fib(x) + " ") }
+
+    println()
+    println()
+    
+    println("isSorted: 1 3 4 5 7 8 -> " + IsSorted.isSorted(Array(1,3,4,5,7,8), (x: Int, y: Int) => x < y))
+    println("isSorted: 1 5 3 4 -> " + IsSorted.isSorted(Array(1,5,3,4), (x:Int, y:Int) => x < y))
+    println("isSorted: 1 -> " + IsSorted.isSorted(Array(1), (x:Int, y:Int) => x < y))
+    println("isSorted: -> " + IsSorted.isSorted(Array(), (x:Int, y:Int) => x < y))
 }
 
 
