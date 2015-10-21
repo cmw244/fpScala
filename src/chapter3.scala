582efbde02c469754dc9dc648f62a41768d81ecd
clen <clen@domeyard.com>
2015-10-21 08:18:08 -0400

    Finish chapter 3

--- src/chapter3.scala
+++ src/chapter3.scala
@@ -83,7 +83,99 @@ object List {
   // Exercise 11
   def sumFL(l: List[Int]) = foldLeft(l, 0)(_ + _)
   def productFL(l: List[Int]) = foldLeft(l, 1)(_ * _)
+
+  // Exercise 12
+  def reverse[A](l: List[A]): List[A] = foldLeft(l,List[A]())((a,b) => Cons(b,a)) 
+
+  // Exercise 13
+  def foldLeftUsingRight[A,B](l: List[A], z: B)(f: (B, A) => B): B = foldRight(l, (b:B) => b)((a,c) => b => c(f(b,a)))(z)
+  def foldRightUsingLeft[A,B](l: List[A], z: B)(f: (A, B) => B): B = foldLeft(reverse(l), z)((a,b) => f(b,a))
+
+  // Exercise 14
+  def append[A](l: List[A], r: List[A]): List[A] = foldRight(l, r)(Cons(_,_))
+  
+  // Exercise 15
+  def concat[A](l: List[List[A]]): List[A] = foldRight(l, List[A]())(append)
+
+  // Exercise 16
+  def addOne(l: List[Int]): List[Int] = {
+    @tailrec
+    def addOneHelper(l: List[Int], acc: List[Int]): List[Int] = l match {
+      case Nil => acc
+      case Cons(hd,tl) => addOneHelper(tl, Cons(hd+1, acc))
+    }
+    addOneHelper(l, Nil)
+  }
+  // Using fold
+  // foldRight(l, List[Int]())((h,t) => Cons(t+1, h))
+
+  // Exercise 17
+  def doubleToString(l: List[Double]): List[String] = {
+             //Alternative to List[String]()
+    foldRight(l, Nil:List[String])((h,t) => Cons(h.toString, t))
+  }
+
+  // Exercise 18
+  def map[A,B](l: List[A])(f: A => B): List[B] = {
+    foldRight(l, Nil:List[B])((h,t) => Cons(f(h), t))
+  }
+ 
+  // Exercise 19
+  def filter[A](l: List[A])(f: A => Boolean): List[A] = {
+    foldRight(l, Nil:List[A])((h,t) => if( f(h) )  Cons(h,t) else  t )
+  }
+
+  // Exercise 20
+  def flatMap[A,B](l: List[A])(f: A => List[B]): List[B] = concat(map(l)(f))
+
+  // Exercise 21
+  def filterFlatMap[A](l: List[A])(f: A => Boolean): List[A] = flatMap(l)(a => if(f(a)) List(a) else Nil)
+
+  // Exercise 22
+  def twoListAdd(l: List[Int], r: List[Int]): List[Int] = (l,r) match {
+    case (Nil,_) => Nil
+    case (_, Nil) => Nil
+    case (Cons(hdA,tlA), Cons(hdB, tlB)) => Cons(hdA+hdB, twoListAdd(tlA, tlB)) 
+  }
+
+  // Exercise 23
+  def twoListAddGeneral[A,B,C](l: List[A], r: List[B])(f: (A,B) => C): List[C] = (l,r) match {
+    case (Nil,_) => Nil
+    case (_, Nil) => Nil
+    case (Cons(hdA,tlA), Cons(hdB, tlB)) => Cons(f(hdA,hdB), twoListAddGeneral(tlA, tlB)(f)) 
+  }
+
+  // Exercise 24
+  // def hasSubsequence[A](l: List[A], sub: List[A]): Boolean = 
+
 }
+
+object Tree {
+  sealed trait Tree[+A]
+  case class Leaf[A](value: A) extends Tree[A]
+  case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+  def size[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 1
+    case Branch(l,r) => 1 + size(l) + size(r)
+  }
+
+  def maxTree(t: Tree[Int]): Int = t  match {
+    case Leaf(x) => x // can you do Leaf(_) => _?
+    case Branch(l,r) => maxTree(l) max maxTree(r)
+  }
+
+  def depth[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 1
+    case Branch(l,r) => (depth(l) max depth(r)) + 1
+  }
+
+  def map[A,B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Leaf(x) => Leaf(f(x))
+    case Branch(l,r) => Branch(map(l)(f), map(r)(f))
+  }
+}
+
 
 object Main extends App {
   import List._
@@ -103,5 +195,7 @@ object Main extends App {
   val lst = List(1,2,3,4,5)
   println("15 -> " + List.sumFL(lst))
   println("120 -> " + List.productFL(lst))
-
+  println("5,4,3,2,1 => " + List.mkString(List.reverse(lst)))
+  val intra = List(List(1,2), List(3,4), List(5,6))
+  println("1,2,3,4,5,6 => " + List.mkString(List.concat(intra)))
 }
